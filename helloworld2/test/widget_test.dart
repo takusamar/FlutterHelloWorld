6d816263e6f199797bb8f7ffb25c3045a92e69ab
taku iwamura <takusamar@gmail.com>
2020-03-06 12:26:28 +0900

    dummy test

--- helloworld2/test/widget_test.dart
+++ helloworld2/test/widget_test.dart
@@ -5,5 +5,11 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter_test/flutter_test.dart';
+
+
 void main() {
+  testWidgets('dummy test', (WidgetTester tester) async {
+    expect(true, true);
+  });
 }
